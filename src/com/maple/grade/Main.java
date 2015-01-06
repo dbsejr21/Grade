a61025e050ae69fd158ea8753fabc891d69d1681
dbsejr21 <dbsejr21@gmail.com>
2015-01-06 18:53:30 +0900

    파일에 출력 구현 중에 백업

--- src/com/maple/grade/Main.java
+++ src/com/maple/grade/Main.java
@@ -1,18 +1,29 @@
 package com.maple.grade;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+			
+		FileWriter fw = new FileWriter("grade.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		StringBuffer sb = new StringBuffer();
 		
 		Calculator c = new Calculator();
-		Printer p = new Printer();
-		Subject[] sa = new Subject[20];
 		
+		Subject[] sa = new Subject[20];
 		sa[0] = new Subject("수학", c.calc(100));
 		
+		Printer p = new Printer();
 		p.print(sa[0]);
 		
+		TextPrinter tp = new TextPrinter();
+		tp.printFile(fw, bw, sb, sa);
+	
 		
 	}
 
