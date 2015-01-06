34d20ebfbb374859e7532183b0c7810157e6e8e9
dbsejr21 <dbsejr21@gmail.com>
2015-01-06 17:30:15 +0900

    계산기능 업그레이드 구현 백업

--- src/com/maple/grade/Main.java
+++ src/com/maple/grade/Main.java
@@ -9,7 +9,11 @@ public class Main {
 		Printer p = new Printer();
 		Subject[] sa = new Subject[20];
 		
-		sa[0] = new Subject("수학", c.calc(100));
+		sa[0] = new Subject("국어", c.calc(100));
+		sa[1] = new Subject("영어", c.calc(80));
+		sa[2] = new Subject("수학", c.calc(70));
+		sa[3] = new Subject("사회", c.calc(60));
+		sa[4] = new Subject("과학", c.calc(50));
 		
 		p.print(sa[0]);
 		
