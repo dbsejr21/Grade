3522e9b24195036acca0f240594f6f95f2da1c61
dbsejr21 <dbsejr21@gmail.com>
2015-01-08 20:21:12 +0900

    첨삭내용 반영

--- src/com/maple/grade/Main.java
+++ src/com/maple/grade/Main.java
@@ -1,46 +1,33 @@
 package com.maple.grade;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 
-		//  Subject 객체배열 초기화
-		Subject[] sa = new Subject[20];
-		for (int i = 0; i < sa.length; i++){
-			sa[i] = new Subject();
-		}
-		
+		// Subject 객체 리스트 초기화
+		ArrayList<Subject> listSubject = new ArrayList<Subject>();
+
 		// 사용자 입력 하드코딩
-		sa[0] = new Subject("국어", 100);
-		sa[1] = new Subject("영어", 80);
-		sa[2] = new Subject("수학", 70);
-		sa[3] = new Subject("사회", 60);
-		sa[4] = new Subject("과학", 50);
-		
+		listSubject.add(new Subject("국어", 100));
+		listSubject.add(new Subject("영어", 80));
+		listSubject.add(new Subject("수학", 70));
+		listSubject.add(new Subject("사회", 60));
+		listSubject.add(new Subject("과학", 50));
+	
 		// 계산
 		Calculator c = new Calculator();
-		c.eachGrade(sa);
+		c.calcEachGrade(listSubject);
 		
 		// 표준출력
 		Printer p = new Printer();
-		p.printEach(sa);
-		p.printSum(sa , c);
-		p.printTotGrade(sa, c, 5);
+		p.printToStdout(listSubject, c);
 
 		// 파일출력
-		FileWriter fw = new FileWriter("grade.txt");
-		BufferedWriter bw = new BufferedWriter(fw);
-		StringBuffer sb = new StringBuffer();
-		
-		TextPrinter tp = new TextPrinter();
-		tp.printFile(fw, bw, sb, sa, c);
-	
+		TextPrinter tp = new TextPrinter("grade.txt");
+		tp.printToFile(listSubject, c);
 		
 	}
-
 }
